944fb53e8dbc455c89b44ae23fc387bdb392b429
Guilherme Gehring <guilherme.gehring@gmail.com>
2019-10-09 08:15:33 -0300

    Adicionado enum de estados, leitura de arquivo texto e começando jogo

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -5,14 +5,15 @@
  */
 package cliente;
 
+import util.Estados;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.IIOException;
+import util.ArquivoTexto;
 
 /**
  *
@@ -56,44 +57,101 @@ public class Cliente {
             Scanner leitura = new Scanner(System.in);
             Cliente cliente = new Cliente("Guilherme");
             String protocolo;
+            String status;
+            ArrayList<Trunfo> cartas = new ArrayList<>();
             //conexao com server
             Socket socket = cliente.conectaComServidor("localhost", 5555);
             //streams de saida e entrada
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
             output.flush();
+            Estados estado = Estados.CONECTADO;
             String msg;
             do {
-                //protocolo
-                //envio
-                System.out.print("0 - Sair\n1 - Login\n2 - Logout\nInforme a opção desejada: ");
-                String opcao = leitura.nextLine();
-                switch (opcao) {
-                    case "0":
-                        output.writeUTF("SAIR");
+                switch (estado) {
+
+                    case CONECTADO:
+                        System.out.print("0 - Sair\n1 - Login\nInforme a opção desejada: ");
+                        String opcao = leitura.nextLine();
+                        switch (opcao) {
+                            case "0":
+                                output.writeUTF("SAIR");
+                                break;
+                            case "1":
+                                String texto = "LOGIN;";
+                                System.out.print("Informe o usuário: ");
+                                texto += "usuario:" + leitura.nextLine();
+                                System.out.print("Informe a senha: ");
+                                texto += ";senha:" + leitura.nextLine();
+                                output.writeUTF(texto);
+                                break;
+                            case "2":
+                                output.writeUTF("LOGOUT");
+                                break;
+                            case "3":
+                                output.writeUTF("JOGAR");
+                                break;
+                            default:
+                                output.writeUTF(opcao);
+                                break;
+
+                        }
                         break;
-                    case "1":
-                        String texto = "LOGIN;";
-                        System.out.print("Informe o usuário: ");
-                        texto += "usuario:" + leitura.nextLine();
-                        System.out.print("Informe a senha: ");
-                        texto += ";senha:" + leitura.nextLine();
-                        output.writeUTF(texto);
+                    case AUTENTICADO:
+                        System.out.print("0 - Logout\n1 - Jogar\n2 - Buscar Cartas\nInforme a opção desejada: ");
+                        opcao = leitura.nextLine();
+                        switch (opcao) {
+                            case "0":
+                                output.writeUTF("LOGOUT");
+                                break;
+                            case "1":
+                                output.writeUTF("JOGAR");
+                                String caminho = "/home/guilherme/NetBeansProjects/ClienteTrunfo/src/util/cartas.txt";
+                                cartas = ArquivoTexto.leitor(caminho);
+                                break;
+                            default:
+                                output.writeUTF(opcao);
+                                break;
+
+                        }
                         break;
-                    case "2":
-                        output.writeUTF("LOGOUT");
+                    case JOGANDO:
+                        Trunfo carta = cartas.remove(0);
+                        System.out.print("Jogador: " + carta.getNome());
+                        System.out.print("1 - Chute: " + carta.getChute());
+                        System.out.print("2 - Defesa: " + carta.getDefesa());
+                        System.out.print("3 - Drible: " + carta.getDrible());
+                        System.out.print("4 - Velocidade: " + carta.getVelocidade());
+                        System.out.print("Informe o atributo: ");
+                        opcao = leitura.nextLine();
+                            
+
                         break;
-                    default:
-                        output.writeUTF(opcao);
-                        break;
-                        
                 }
                 output.flush();
                 //recebimento
                 msg = input.readUTF();
                 System.out.println("Servidor respondeu: "  + msg);
                 protocolo = msg.split(":")[0];
-            } while (!protocolo.equals("SAIRRESPONSE"));
+                status = msg.split(":")[1];
+                
+                if (status.equals("OK")) {
+                    switch (protocolo) {
+                        case "LOGINRESPONSE":
+                            estado = Estados.AUTENTICADO;
+                            break;
+                        case "LOGOUTRESPONSE":
+                            estado = Estados.CONECTADO;
+                            break;
+                        case "SAIRRESPONSE":
+                            estado = Estados.DESCONECTADO;
+                            break;
+                        case "JOGARRESPONSE":
+                            estado = Estados.JOGANDO;
+                            break;
+                    }
+                }
+            } while (!estado.equals(Estados.DESCONECTADO));
             //fecha streams e conexão
             output.close();
             input.close();
---
+++ src/cliente/Trunfo.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author 20171pf.cc0178
+ */
+public class Trunfo {
+    private String nome;
+    private String tipo;
+    private int defesa;
+    private int drible;
+    private int velocidade;
+    private int chute;
+
+    public Trunfo() {
+    }
+
+    public Trunfo(String nome, String tipo, int defesa, int drible, int velocidade, int chute) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.defesa = defesa;
+        this.drible = drible;
+        this.velocidade = velocidade;
+        this.chute = chute;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    public int getDrible() {
+        return drible;
+    }
+
+    public void setDrible(int drible) {
+        this.drible = drible;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getChute() {
+        return chute;
+    }
+
+    public void setChute(int chute) {
+        this.chute = chute;
+    }
+
+    @Override
+    public String toString() {
+        return nome + "{" + tipo + ", defesa:" + defesa + ", drible:" + drible + ", chute:" + chute + "}";
+    }
+    
+    
+}
